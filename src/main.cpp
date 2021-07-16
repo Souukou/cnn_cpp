a2bb7e7507e774d829bc3a9af95a6cc3000321b6
Yuhang Song <i@syh.one>
2021-07-16 14:51:25 +0800

    新增命令行输入参数

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 #include "model.h"
+
 #define INTEL
 #ifdef INTEL
 #include <mkl.h>
 #endif
+
 void print_tensor(tensor& x);
-double get_time(){
-#ifdef INTEL
-    return dsecnd();
-#endif
-#ifndef INTEL
-    return omp_get_wtime();
-#endif
-}
-int main()
+double get_time();
+void usage(char* prog_name);
+void get_args(int argc, char* argv[], int &n, int &thread_count, bool &verbose);
+
+int main(int argc, char* argv[])
 {
+    int thread_count = 0, n; // auto thread by default
+    bool verbose = false;
+    get_args(argc, argv, n, thread_count, verbose);
+
     nn::Model m = nn::Model();
 
     m.add_layer( new nn::Conv2D(4, 1, 3, 3) );
@@ -30,38 +32,27 @@ int main()
     m.magic_train();
 
     // test input
-    tensor_4d test = nn::new_tensor_4d(10000, 1, 16, 16);
+    tensor_4d test = nn::new_tensor_4d(n, 1, 16, 16);
 
     nn::random_tensor_4d(test);
-
-    //print_tensor(test[0]);
+    
+    if(verbose)
+        print_tensor(test[0]);
 
     double start, end;
     int LOOP_COUNT = 5;
     tensor_4d result;
-    // auto threads
+
 
     start = get_time();
     for (int i=0; i<LOOP_COUNT; ++i)
-         result= m.predic(test);
+         result= m.predic(test, thread_count);
     end = get_time();
-    print_tensor(result[0]);
+
+    if(verbose)
+        print_tensor(result[0]);
 
     std::cout << "Auto Threads Time Used: " << (end - start) / LOOP_COUNT << std::endl;
-
-    // two threads
-    start = get_time();
-    for (int i=0; i<LOOP_COUNT; ++i)
-        m.predic(test, 2);
-    end = get_time();
-    std::cout << "Two Threads Time Used: " << (end - start) / LOOP_COUNT << std::endl;
-
-    // single threads (serialize)
-    start = get_time();
-    for (int i=0; i<LOOP_COUNT; ++i)
-        m.predic(test, 1);
-    end = get_time();
-    std::cout << "Serialize Time Used: " << (end - start) / LOOP_COUNT << std::endl;
 
 
     return 0;
@@ -77,3 +68,37 @@ void print_tensor(tensor& x){
         std::cout << std::endl;
     }
 }
+
+double get_time(){
+#ifdef INTEL
+    return dsecnd();
+#endif
+#ifndef INTEL
+    return omp_get_wtime();
+#endif
+}
+
+void usage(char* prog_name) {
+    fprintf(stderr, "usage:   %s <n> <t> <v|q>\n", prog_name);
+    fprintf(stderr, "   n:   number of inputs\n");
+    fprintf(stderr, "   t:   number of threads, default auto thread\n");
+    fprintf(stderr, "  'v':  verbose, print first tensor\n");
+    fprintf(stderr, "  'q':  quiet\n");
+} 
+
+void get_args(int argc, char* argv[], int &n, int &thread_count, bool &verbose) {
+    if (argc < 2 ) {
+        usage(argv[0]);
+        exit(0);
+    }
+    n = atoi(argv[1]);
+    if (argc >= 3)
+        thread_count = atoi(argv[2]);
+    if (argc >= 4)
+        verbose = *argv[3] == 'v';
+    if (n <= 0 || thread_count < 0) {
+        usage(argv[0]);
+        exit(0);
+    }
+} 
+
